04115e15414ba9ede45c33bdc80c18fb2de539c2
Daniel Lamprecht <lamprecht.daniel@outlook.com>
2019-04-16 13:48:48 +0200

    Initial Commit

---
+++ Stopwatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by Daniel Lamprecht on 16.04.19.
+//  Copyright © 2019 Daniel Lamprecht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
